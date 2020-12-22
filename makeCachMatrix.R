eb3504b859a5a8f3ddb26fababf1ed17672bcda3
myfb2020 <75948741+myfb2020@users.noreply.github.com>
2020-12-23 00:24:07 +0300

    Add files via upload

---
+++ makeCachMatrix.R
@@ -0,0 +1,23 @@
+makeCachMatrix <- function(x = matrix()){
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() {x}
+      setInverse <- function(inverse) {inv <<- inverse}
+      getInverse <- function() {inv}
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){
+      inv <- x$getInverse()
+      if(!is.null(inv)){
+            message("getting cached data!")
+            return(inv)
+      }
+      dat <- x$get()
+      inv <- solve(dat, ...)
+      x$setInverse(inv)
+      inv
+}      get
